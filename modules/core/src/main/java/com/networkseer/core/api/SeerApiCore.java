616d34971fcf3073f96651e408e54ff7be56f6a5
Ayyoob Hamza <ayyoobhamza@gmail.com>
2018-01-27 03:21:38 +1100

    added swagger docs

--- modules/core/src/main/java/com/networkseer/core/api/SeerApiCore.java
+++ modules/core/src/main/java/com/networkseer/core/api/SeerApiCore.java
@@ -1,11 +1,14 @@
 package com.networkseer.core.api;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.networkseer.common.SeerApiPlugin;
 import com.networkseer.core.config.SeerConfiguration;
 import com.networkseer.core.health.monitor.HealthRestApiController;
 import io.dropwizard.Application;
 import io.dropwizard.lifecycle.ServerLifecycleListener;
 import io.dropwizard.setup.Environment;
+import io.swagger.jaxrs.config.BeanConfig;
+import io.swagger.jaxrs.listing.ApiListingResource;
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
@@ -22,14 +25,21 @@ public class SeerApiCore extends Application<SeerConfiguration> {
 	@Override
 	public void run(SeerConfiguration configuration, Environment environment) throws Exception {
 		log.info("Deploying services...");
+		environment.jersey().register(new ApiListingResource());
 		environment.jersey().register(new HealthRestApiController());
-
+		environment.getObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL);
 		ServiceLoader<SeerApiPlugin> loader = ServiceLoader.load(SeerApiPlugin.class);
 		Iterator<SeerApiPlugin> it = loader.iterator();
 		while (it.hasNext()) {
 			SeerApiPlugin seerApi = it.next();
 			environment.jersey().register(seerApi);
 		}
+		BeanConfig config = new BeanConfig();
+		config.setTitle("Network Seer");
+		config.setVersion("1.0.0");
+		config.setResourcePackage("com.networkseer");
+		config.setScan(true);
+
 
 		environment.lifecycle().addServerLifecycleListener(new ServerLifecycleListener() {
 			@Override
